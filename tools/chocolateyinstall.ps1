2b815da5dbed075e97e1a5dd5c948e25a927c589
alirdn <alireza.dabirinejad@live.com>
2018-07-10 02:55:45 +0430

    First release. Version 1.1.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  file        = "$toolsPath\archives\windows-kill_1.1.0_release_x86.zip"
+  file64      = "$toolsPath\archives\windows-kill_1.1.0_release_x64.zip"
+  destination = $toolsPath
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+Remove-Item $toolsPath\archives\*.zip -ea 0
+Remove-Item $toolsPath\archives -ea 0
