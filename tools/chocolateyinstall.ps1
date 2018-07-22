171cc8391fd7907000d643787bf19e84ec958855
alirdn <alireza.dabirinejad@live.com>
2018-07-23 02:16:15 +0430

    Bump version 1.1.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsPath\archives\windows-kill_Win32_1.1.3_lib_release.zip"
-  file64      = "$toolsPath\archives\windows-kill_x64_1.1.3_lib_release.zip"
+  file        = "$toolsPath\archives\windows-kill_Win32_1.1.4_lib_release.zip"
+  file64      = "$toolsPath\archives\windows-kill_x64_1.1.4_lib_release.zip"
   destination = $toolsPath
 }
 
